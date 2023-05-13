dbd0386a1cf628916d63a08634c616f67b5f7bda
nagaraj <raj.nagaraj1990@gmail.com>
2023-05-13 12:37:14 -0400

    Added web socket server

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+let ws = new WebSocket("ws://localhost:8080");
+
 const save = async () => {
   await fetch("api/save", {
     method: "POST",
@@ -7,14 +9,18 @@ const save = async () => {
       "Content-Type": "application/json",
     },
   });
+  ws.send("Save done");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("parsed");
   let source = new EventSource("http://localhost:3000/sse");
   source.onmessage = function (event) {
-    console.log(event);
-    console.log("here");
+    console.log("SSE", event);
+  };
+
+  ws.onmessage = function (message) {
+    console.log("web socket", message);
   };
 
   document.getElementById("submit").addEventListener("click", () => {
